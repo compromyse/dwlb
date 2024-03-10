7450f787299effd40aff92eaa4c18cad269813cc
Raghuram Subramani <raghus2247@gmail.com>
2024-03-10 08:32:19 +0530

    update

--- config.def.h
+++ config.def.h
@@ -3,7 +3,7 @@ static bool ipc = false;
 // initially hide all bars
 static bool hidden = false;
 // initially draw all bars at the bottom
-static bool bottom = false;
+static bool bottom = true;
 // hide vacant tags
 static bool hide_vacant = false;
 // vertical pixel padding above and below text
@@ -17,20 +17,20 @@ static bool custom_title = false;
 // scale
 static uint32_t buffer_scale = 1;
 // font
-static char *fontstr = "monospace:size=16";
+static char *fontstr = "UbuntuMono Nerd Font Mono:size=16";
 // tag names
 static char *tags_names[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 // set 16-bit colors for bar
 // 8-bit color can be converted to 16-bit color by simply duplicating values e.g
 // 0x55 -> 0x5555, 0xf1 -> 0xf1f1
-static pixman_color_t active_fg_color = { .red = 0xeeee, .green = 0xeeee, .blue = 0xeeee, .alpha = 0xffff, };
-static pixman_color_t active_bg_color = { .red = 0x0000, .green = 0x5555, .blue = 0x7777, .alpha = 0xffff, };
+static pixman_color_t active_fg_color = { .red = 0x2727, .green = 0x2727, .blue = 0x2727, .alpha = 0xffff, };
+static pixman_color_t active_bg_color = { .red = 0xf2f2, .green = 0xf0f0, .blue = 0xfafa, .alpha = 0xffff, };
 static pixman_color_t occupied_fg_color = { .red = 0xeeee, .green = 0xeeee, .blue = 0xeeee, .alpha = 0xffff, };
 static pixman_color_t occupied_bg_color = { .red = 0x0000, .green = 0x5555, .blue = 0x7777, .alpha = 0xffff, };
 static pixman_color_t inactive_fg_color = { .red = 0xbbbb, .green = 0xbbbb, .blue = 0xbbbb, .alpha = 0xffff, };
 static pixman_color_t inactive_bg_color = { .red = 0x2222, .green = 0x2222, .blue = 0x2222, .alpha = 0xffff, };
 static pixman_color_t urgent_fg_color = { .red = 0x2222, .green = 0x2222, .blue = 0x2222, .alpha = 0xffff, };
 static pixman_color_t urgent_bg_color = { .red = 0xeeee, .green = 0xeeee, .blue = 0xeeee, .alpha = 0xffff, };
-static pixman_color_t middle_bg_color = { .red = 0x2222, .green = 0x2222, .blue = 0x2222, .alpha = 0xffff, };
-static pixman_color_t middle_bg_color_selected = { .red = 0x0000, .green = 0x5555, .blue = 0x7777, .alpha = 0xffff, };
+static pixman_color_t middle_bg_color = { .red = 0x2727, .green = 0x2727, .blue = 0x2727, .alpha = 0xffff, };
+static pixman_color_t middle_bg_color_selected = { .red = 0xf2f2, .green = 0xf0f0, .blue = 0xfafa, .alpha = 0xffff, };
